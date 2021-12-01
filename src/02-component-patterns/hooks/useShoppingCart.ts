0430402cb44d6507ff0e66203b666ff21ebe5a84
Pablo Ibarra <pabloo_ibarra98@hotmail.com>
2021-12-01 12:26:26 -0600

    Control props alt listo

--- src/02-component-patterns/hooks/useShoppingCart.ts
+++ src/02-component-patterns/hooks/useShoppingCart.ts
@@ -6,30 +6,16 @@ export const useShoppingCart = () => {
 
   const onProductCountChange = ({ count, product }: { count: number, product: Product }) => {
     setShoppingCart( oldShoppingCart => {
+      if( count === 0 ) {
+        const { [product.id]: toDelete, ...rest } = oldShoppingCart;
 
-      const productInCart: ProductInCart = oldShoppingCart[product.id] || { ...product, count: 0 };
-
-      if( Math.max( productInCart.count + count, 0 ) > 0 ) {
-        productInCart.count += count;
-        return { 
-          ...oldShoppingCart,
-          [product.id]: productInCart
-        }
+        return rest;
       }
 
-      const { [product.id]: toDelete, ...rest } = oldShoppingCart;
-      return rest;
-
-      // if( count === 0 ) {
-      //   const { [product.id]: toDelete, ...rest } = oldShoppingCart;
-
-      //   return rest;
-      // }
-
-      // return {
-      //   ...oldShoppingCart,
-      //   [product.id]: { ...product, count }
-      // }
+      return {
+        ...oldShoppingCart,
+        [product.id]: { ...product, count }
+      }
     });
   }
 
